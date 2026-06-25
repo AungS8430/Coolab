e543b797d7ba39828f1fdc29456b465079dec4ce
aungs8430 <aungshome@outlook.com>
2026-06-25 22:48:16 +0900

    Fixed indentation

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,33 @@
-import Editor from './components/Editor';
+import { useFilesStore } from './store/files';
+
+import { Editor } from './components/Editor';
+import { Sidebar } from './components/Sidebar';
+import { Tabs } from './components/Tabs';
+import { StatusBar } from './components/StatusBar';
 
 function App() {
+  const { activeFile, updateFileContent, updateCursorPosition } = useFilesStore();
+
+  const file = activeFile();
   return (
     <div className="App">
-      <Editor />
+      {
+        file ? (
+          <Editor
+            key={file.id}
+            value={file.content}
+            language={file.language}
+            theme='vs-dark'
+            onChange={(value) => updateFileContent(file.id, value)}
+            onCursorMove={(position) => updateCursorPosition(file.id, position)}
+          />
+        ) : (
+          <div>No file opened</div>
+        )
+      }
+      <Sidebar />
+      <Tabs />
+      <StatusBar />
     </div>
   )
 }
