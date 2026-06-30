ae2f8265879274a11462f147218f85772d389ce4
aungs8430 <aungshome@outlook.com>
2026-06-30 20:48:23 +0900

    Added outside click hook

---
+++ src/hooks/useClickOutside.ts
@@ -0,0 +1,16 @@
+import { useEffect, useRef } from 'react';
+
+function useClickOutside(callback: () => void) {
+  const ref = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    function handleClick(e: MouseEvent) {
+      if (ref.current && !ref.current.contains(e.target as Node)) callback();
+    }
+
+    document.addEventListener('mousedown', handleClick);
+    return () => document.removeEventListener('mousedown', handleClick);
+  }, [callback]);
+
+  return ref;
+}
